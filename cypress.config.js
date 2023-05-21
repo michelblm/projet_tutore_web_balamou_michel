80ef1b723c4b3c7523c98300bcc9745635a06cd8
michelblm <mbalamou@hotmail.fr>
2023-05-21 20:55:06 +0200

    Commit Config

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,32 @@
+const cucumber = require('cypress-cucumber-preprocessor').default;
+
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: "cypress-mochawesome-reporter",
+  video: false,
+
+  reporterOptions: {
+    charts: true,
+
+    reportPageTitle: "Cypress Inline Reporter",
+
+    embeddedScreenshots: true,
+    inlineAssets: true, //Adds the asserts inline
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      on('file:preprocessor', cucumber());
+      screenshotOnRunFailure = true;
+      require("cypress-mochawesome-reporter/plugin")(on);
+    },
+    env: {
+      url:
+        process.env.CYPRESS_ENV_URL ||
+        "http://opencruise-ok.sogeti-center.cloud",
+      username: "admin@test.com",
+      password: "Sogeti33"
     },
   },
 });
